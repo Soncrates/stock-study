fc52210dbfc66d8e81d6913a09b240afcdb5e664
Soncrates <emersoncus@yahoo.com>
2015-03-19 18:25:01 -0400

    Update scrapers.py

--- scrapers.py
+++ scrapers.py
@@ -18,27 +18,29 @@ class YahooScraper(BaseScraper) :
         yield token
 
 class NasdaqScraper(BaseScraper) :
-    def __init__(self,data_get,data_parse,data_formatter,exchange_list=None,unwanted_keys_list=None) : 
-        BaseScraper.__init__( self,data_get,data_parse,data_formatter)
-        self.exchanges=["nyse", "nasdaq"]
-        self.unwanted_keys=['Summary Quote','MarketCap','LastSale','IPOyear']
-        if exchange_list is not None  : self.exchanges = exchange_list
-        if unwanted_keys_list is not None  : self.unwanted_keys = unwanted_keys_list
-    def __call__(self,exchange_list=None,unwanted_keys_list=None) :
-      exchanges = self.exchanges
-      unwanted_keys = self.unwanted_keys
-      if exchange_list is not None  : exchanges = exchange_list
-      if unwanted_keys_list is not None  : unwanted_keys = unwanted_keys_list
-      ret = {}
-      for exchange in exchanges :
-        data = self.get_data(exchange)
-        for ele in self.parse_data(data) :
-          if self.data_formatter is not None :
-             ele = self.data_formatter(ele,unwanted_keys,exchange)
-          symbol=ele['Symbol']
-          del ele['Symbol']
-          ret[symbol] = ele
-      return ret
+  def __init__(self,data_get,data_parse,data_formatter,exchange_list=None,unwanted_keys_list=None) : 
+    BaseScraper.__init__( self,data_get,data_parse,data_formatter)
+    self.exchanges=["nyse", "nasdaq"]
+    self.unwanted_keys=['Summary Quote','MarketCap','LastSale','IPOyear']
+    if exchange_list is not None  : self.exchanges = exchange_list
+    if unwanted_keys_list is not None  : self.unwanted_keys = unwanted_keys_list
+  def __call__(self,exchange_list=None,unwanted_keys_list=None) :
+    import pandas as pd
+    exchanges = self.exchanges
+    unwanted_keys = self.unwanted_keys
+    if exchange_list is not None  : exchanges = exchange_list
+    if unwanted_keys_list is not None  : unwanted_keys = unwanted_keys_list
+    ret = None
+    for exchange in exchanges :
+      data = self.get_data(exchange)
+      data = self.parse_data(data)
+      if self.data_formatter is not None :
+        data = self.data_formatter(data,unwanted_keys,exchange)
+      if ret is None : ret = data
+      else : 
+        temp = dict(list(ret.items()) + list(data.items()))
+        ret = pd.DataFrame.from_dict(temp)
+    return ret
         
 nasdaq = NasdaqScraper(get_nasdaq_csv,parse_csv,format_nasdaq)
 yahoo_cash_flow = YahooScraper(get_yahoo_cash_flow,format_as_soup,parse_yahoo)
