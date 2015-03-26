11a6f5118f2008181a29f1289b079216605f84bf
Soncrates <emersoncus@yahoo.com>
2015-03-26 11:33:43 -0400

    Update scraper-util.py

--- data/scraper-util.py
+++ data/scraper-util.py
@@ -21,25 +21,6 @@ def get_nasdaq_csv(exchange) : # from nasdaq.com
   url = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=%s&render=download" % exchange
   return invoke_url(url,headers,raw=True)
 
-def format_nasdaq(ret,unwanted_keys,exchange) :
-  import pandas as pd
-  ret['Exchange'] = pd.Series(exchange, index=ret.index)
-  if unwanted_keys is not None  : 
-    for key in unwanted_keys:
-      if key in ret.columns.tolist() : ret.drop(key, 1)
-  temp = ret.to_dict()
-  if unwanted_keys is not None  : 
-    for key in unwanted_keys:
-      if key in temp.keys() : del temp[key]
-  if len(temp.keys()) < len(ret.columns.tolist()) :
-    ret = pd.DataFrame.from_dict(temp)
-  return ret
-def parse_csv(csv) :
-    import pandas as pd
-    import io
-    ret = pd.read_csv(io.BytesIO(csv), encoding='utf8', sep=",",index_col="Symbol")
-    return ret
-
 def get_morningstar_cash_flow(exchange_code, ticker) :
   url = 'http://financials.morningstar.com/ajax/ReportProcess4HtmlAjax.html?&t=%s:%s&region=usa&culture=en-US&cur=USD&reportType=cf&period=12&dataType=A&order=asc&columnYear=5&rounding=3&view=raw&r=963470&callback=jsonp%d&_=%d' % (exchange_code, ticker, int(time.time()), int(time.time()+150))
   return invoke_url(url)
@@ -70,14 +51,3 @@ def format_yahoo_finance_rss(rss) :
   import xmltodict,re
   for item in re.findall(r'<item>(\w+)<\/item>', rss) :
     yield xmltodict.parse(item)
-def get_yahoo_historical(stock,year,strict=True) :
-    import pandas.io.data as pdd
-    try :
-        ret = pdd.DataReader(stock, data_source='yahoo', start='{}/1/1'.format(year))
-    except IOError :
-        return None
-    if not strict : return ret
-    first = ret.index.tolist()[0]
-    if year != first.year : return None
-    if 1 != first.month : return None
-    return ret
