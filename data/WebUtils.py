bde44190e9f54c4b57d3a96812af9ce89942ddf1
Soncrates <emersoncus@yahoo.com>
2015-04-16 18:57:11 -0400

    Concurreny and Caching Yahoo stock service
    Now when downloading daily stock information, it can be accessed concurrently and access cached data.
    As bonus, you can pass a single ticker symbol or in a collection

--- data/WebUtils.py
+++ data/WebUtils.py
@@ -142,3 +142,30 @@ class YahooParse(object) :
         if year != first.year : return None
         if 1 != first.month : return None
         return ret
+    class StockDailyService(object) :
+      def __init__(self,service=YahooParse.get_stock_daily,strict=True) :
+        y1,y2,r = TimeUtil.get_year_parameters()
+        self.default_year = y1
+        self.strict = strict
+        self.service = service
+      def __call__(self,stock, year=None) :
+        if year is None : year = self.default_year
+        return self.service(stock,year,self.strict)
+    class StockDailyCache(object) :
+      from functools import lru_cache
+      def __init__(self,service = YahooParse.StockDailyService()) :
+        self.fresh = {}
+        self.cache = {}
+        self.service = service
+      @lru_cache(maxsize=100)
+      def __call__(self,stock) :
+        if stock not in self.cache.keys() or not self.fresh[stock](): 
+          self.cache[stock] = self.service(stock)
+          self.fresh[stock] = TimeUtil.ExpireTimer(24*60) 
+        return self.cache[stock]
+    class StockDaily(object) :
+      def __init__(self, service = ConcurrentActor(YahooParse.StockDailyCache())) :
+        self.service = service
+      def __call__(self,stock) :
+        return self.service(stock)
+        
