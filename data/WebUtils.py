699b8ac57b9ff115f95a44eae233d31a9e265d0c
Soncrates <emersoncus@yahoo.com>
2015-04-16 18:59:28 -0400

    Update WebUtils.py

--- data/WebUtils.py
+++ data/WebUtils.py
@@ -130,42 +130,4 @@ class YahooParse(object) :
         text = YahooParse.finance_2(text)
         if isinstance(text, str) :  yield text
         else : yield str(text*factor)
-    @staticmethod
-    def get_stock_daily(stock,year,strict=True) :
-        import pandas.io.data as pdd
-        try :
-            ret = pdd.DataReader(stock, data_source='yahoo', start='{}/1/1'.format(year))
-        except IOError :
-            return None
-        if not strict : return ret
-        first = ret.index.tolist()[0]
-        if year != first.year : return None
-        if 1 != first.month : return None
-        return ret
-    class StockDailyService(object) :
-      def __init__(self,service=YahooParse.get_stock_daily,strict=True) :
-        y1,y2,r = TimeUtil.get_year_parameters()
-        self.default_year = y1
-        self.strict = strict
-        self.service = service
-      def __call__(self,stock, year=None) :
-        if year is None : year = self.default_year
-        return self.service(stock,year,self.strict)
-    class StockDailyCache(object) :
-      from functools import lru_cache
-      def __init__(self,service = YahooParse.StockDailyService()) :
-        self.fresh = {}
-        self.cache = {}
-        self.service = service
-      @lru_cache(maxsize=100)
-      def __call__(self,stock) :
-        if stock not in self.cache.keys() or not self.fresh[stock](): 
-          self.cache[stock] = self.service(stock)
-          self.fresh[stock] = TimeUtil.ExpireTimer(24*60) 
-        return self.cache[stock]
-    class StockDaily(object) :
-      def __init__(self, service = ConcurrentActor(YahooParse.StockDailyCache())) :
-        self.service = service
-      def __call__(self,stock) :
-        return self.service(stock)
         
