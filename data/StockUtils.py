36955ba0d50f2dd4a5ad1a45c80b35c30adea7bb
Soncrates <emersoncus@yahoo.com>
2015-04-17 18:48:47 -0400

    Update StockUtils.py

--- data/StockUtils.py
+++ data/StockUtils.py
@@ -40,40 +40,7 @@ class YahooFinance(object) :
       import xmltodict,re
       for item in re.findall(r'<item>(\w+)<\/item>', rss) :
         yield xmltodict.parse(item)
-class NasDaq(object) :
-    @staticmethod
-    def get_csv(exchange) : # from nasdaq.com
-      headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-      }
-      url = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=%s&render=download" % exchange
-      return WebUtils.invoke_url(url,headers,raw=True)
-    @staticmethod
-    def parse_csv_into_panda(csv) :
-        import pandas as pd
-        import io
-        ret = pd.read_csv(io.BytesIO(csv), encoding='utf8', sep=",",index_col="Symbol")
-        return ret
-    @staticmethod
-    def format_panda(ret,unwanted_keys,exchange) :
-      import pandas as pd
-      ret['Exchange'] = pd.Series(exchange, index=ret.index)
-      if unwanted_keys is not None  : 
-        for key in unwanted_keys:
-          if key in ret.columns.tolist() : ret.drop(key, 1)
-      temp = ret.to_dict()
-      if unwanted_keys is not None  : 
-        for key in unwanted_keys:
-          if key in temp.keys() : del temp[key]
-      if len(temp.keys()) < len(ret.columns.tolist()) :
-        ret = pd.DataFrame.from_dict(temp)
-      return ret    
+  
 class StockUtils(object) :
     @staticmethod
     def get_exchange_code(stock) :
