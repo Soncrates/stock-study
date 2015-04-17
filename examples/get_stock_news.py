678f6ad6df75ee6058f6232b18af6ff75ff50b69
Soncrates <emersoncus@yahoo.com>
2015-04-17 19:26:02 -0400

    Update get_stock_news.py

--- examples/get_stock_news.py
+++ examples/get_stock_news.py
@@ -3,20 +3,24 @@ yahoo_income_statement = ScraperUtil.Yahoo(YahooFinance.get_income_statement,Web
 yahoo_balance_sheet = ScraperUtil.Yahoo(YahooFinance.get_balance_sheet,WebUtils.format_as_soup,YahooParse.finance)
 yahoo_analyst_estimates_soup = ScraperUtil.Yahoo(YahooFinance.get_analyst_estimates,WebUtils.format_as_soup,YahooParse.finance)
 
+yahoo_cash_flow = CacheService(yahoo_cash_flow)
+yahoo_income_statement = CacheService(yahoo_income_statement)
+yahoo_balance_sheet = CacheService(yahoo_balance_sheet)
+yahoo_analyst_estimates_soup = CacheService(yahoo_analyst_estimates_soup)
+
 stocks = CacheService(NasDaqPandaDataService())
 news = CacheService(YQL.NewsFeed())
-temp = {}
-for symbol in stocks().index.values :
-    if symbol[0] not in temp.keys() :
-        temp[symbol[0]] = []
-    temp[symbol[0]].append(symbol)
-for k in sorted(temp.keys()) :
-    print ("{} - {}".format(k,len(temp[k])))
+symbol_list_list = CollectionUtils.chunkify(sorted(stocks().index.values))
+
+for symbol_list in symbol_list_list :
+    print(symbol_list)
+    print(news(symbol_list))
+    break
 
 for symbol in stocks().index.values:
     print(symbol)
     print(news(symbol))
-    break;
+    break
 for symbol in stocks().index.values:
     print(symbol)
     for token in yahoo_cash_flow(symbol) :
