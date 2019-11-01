4d5dee2970fe640a5ef0dabe29a991cb897d1f66
Soncrates <emersoncus@yahoo.com>
2019-11-01 10:42:40 -0400

    Fixed template.py

--- bin/template.py
+++ bin/template.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python
 
 import logging
+import sys
+from libCommon import log_exception
+from libDebug import trace
 
-def main(file_list, ini_list) :
-    try :
-        return _main(file_list, ini_list)
-    except Exception as e :
-        logging.error(e, exc_info=True)
-
-def _main(file_list, ini_list) : pass
+@trace
+@log_exception
+def main(file_list, ini_list) : pass
 
 if __name__ == '__main__' :
+   import logging
+   from libCommon import ENVIRONMENT
 
-   from glob import glob
-   import os,sys
-   from libCommon import TIMER
-
-   pwd = os.getcwd()
-
-   dir = pwd.replace('bin','log')
-   name = sys.argv[0].split('.')[0]
-   log_filename = '{}/{}.log'.format(dir,name)
+   env = ENVIRONMENT()
+   log_filename = '{pwd_parent}/log/{name}.log'.format(**vars(env))
    log_msg = '%(module)s.%(funcName)s(%(lineno)s) %(levelname)s - %(message)s'
-   logging.basicConfig(filename=log_filename, filemode='w', format=log_msg, level=logging.INFO)
+   #logging.basicConfig(filename=log_filename, filemode='w', format=log_msg, level=logging.INFO)
+   logging.basicConfig(stream=sys.stdout, format=log_msg, level=logging.DEBUG)
 
-   local = pwd.replace('bin','local')
-   ini_list = glob('{}/*.ini'.format(local))
-   file_list = glob('{}/historical_prices/*pkl'.format(local))
+   ini_list = env.list_filenames('local/*.ini')
+   file_list = env.list_filenames('local/historical_prices/*pkl')
 
-   logging.info("started {}".format(name))
-   elapsed = TIMER.init()
+
    main(file_list,ini_list)
-   logging.info("finished {} elapsed time : {}".format(name,elapsed()))
 
